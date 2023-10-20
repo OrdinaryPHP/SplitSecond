ee6e549c0c689e1d485b3537bfe5d986465266c3
Brandon Jackson <brandonja991@gmail.com>
2023-10-20 15:09:59 -0500

    Add SplitSecondUnit testing

---
+++ tests/SplitSecondUnitTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Ordinary\SplitSecond;
+
+use PHPUnit\Framework\TestCase;
+
+class SplitSecondUnitTest extends TestCase
+{
+    public static function splitSecondEnumProvider(): \Generator
+    {
+        foreach (SplitSecondUnit::cases() as $case) {
+            yield [$case];
+        }
+    }
+
+    /** @dataProvider splitSecondEnumProvider */
+    public function testPerSecond(SplitSecondUnit $case)
+    {
+        $expected = match ($case) {
+            SplitSecondUnit::Millisecond => 1_000,
+            SplitSecondUnit::Microsecond => 1_000_000,
+            SplitSecondUnit::Nanosecond => 1_000_000_000,
+        };
+
+        self::assertSame($expected, $case->perSecond());
+    }
+
+    /** @dataProvider splitSecondEnumProvider */
+    public function testMaxInSecond(SplitSecondUnit $case)
+    {
+        $expected = match ($case) {
+            SplitSecondUnit::Millisecond => 999,
+            SplitSecondUnit::Microsecond => 999_999,
+            SplitSecondUnit::Nanosecond => 999_999_999,
+        };
+
+        self::assertSame($expected, $case->maxInSecond());
+    }
+
+    /** @dataProvider splitSecondEnumProvider */
+    public function testAbbr(SplitSecondUnit $case)
+    {
+        $expected = match ($case) {
+            SplitSecondUnit::Millisecond => 'ms',
+            SplitSecondUnit::Microsecond => 'μs',
+            SplitSecondUnit::Nanosecond => 'ns',
+        };
+
+        self::assertSame($expected, $case->abbr());
+    }
+
+    /** @dataProvider splitSecondEnumProvider */
+    public function testDecimalPrecision(SplitSecondUnit $case)
+    {
+        $expected = match ($case) {
+            SplitSecondUnit::Millisecond => 3,
+            SplitSecondUnit::Microsecond => 6,
+            SplitSecondUnit::Nanosecond => 9,
+        };
+
+        self::assertSame($expected, $case->decimalPrecision());
+    }
+}
